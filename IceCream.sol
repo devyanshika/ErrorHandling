d1a439d3162515243718cde1ca939de0af8847df
devyanshika <106855016+devyanshika@users.noreply.github.com>
2024-06-26 01:47:08 +0530

    IceCream.sol

---
+++ IceCream.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+contract IceCream{
+    uint256 public IceCreamStock;
+    //require()
+    function addIceCream (uint256 _amount) public {
+        require(_amount > 0, "amount to be greater than 0");
+        IceCreamStock = _amount;
+    }
+
+    //assert()
+    function assertIceCream() public view returns (string memory) {
+        assert(IceCreamStock != 0);
+        return "Ice Cream Stock is not zero.";
+    }
+
+    //revert()
+    function revertIceCream(uint256 _amount)public pure{
+        if (_amount % 2 !=0){
+            revert("Amount to be even");
+        }
+    }
+}
